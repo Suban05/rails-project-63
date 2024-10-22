31dd65e1d82f6f39c7997b82383677e76c3ee246
Suban05 <anatolyb94@gmail.com>
2024-10-23 01:06:55 +0500

    change paths for `autoload`

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -7,12 +7,12 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
 
-  autoload(:Tag, "./lib/hexlet_code/tag.rb")
-  autoload(:FormItem, "./lib/hexlet_code/form_item.rb")
-  autoload(:Form, "./lib/hexlet_code/form.rb")
-  autoload(:FormBuilder, "./lib/hexlet_code/form_builder.rb")
-  autoload(:HtmlRenderer, "./lib/hexlet_code/html_renderer.rb")
-  autoload(:FormRenderer, "./lib/hexlet_code/form_renderer.rb")
+  autoload(:Tag, "hexlet_code/tag.rb")
+  autoload(:FormItem, "hexlet_code/form_item.rb")
+  autoload(:Form, "hexlet_code/form.rb")
+  autoload(:FormBuilder, "hexlet_code/form_builder.rb")
+  autoload(:HtmlRenderer, "hexlet_code/html_renderer.rb")
+  autoload(:FormRenderer, "hexlet_code/form_renderer.rb")
 
   def self.form_for(object, options = {})
     form_builder = FormBuilder.new(object, options)
