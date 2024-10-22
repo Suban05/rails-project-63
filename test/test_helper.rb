c9daeb7676ee369868de58922650461a83115740
Suban05 <anatolyb94@gmail.com>
2024-10-22 20:12:12 +0500

    load fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,11 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "hexlet_code"
-
+require "yaml"
 require "minitest/autorun"
 require "rubocop-minitest"
 require "rubocop-rake"
+
+def load_fixture(name)
+  YAML.load_file("./test/fixtures/#{name}.yml")[name.to_s]
+end
