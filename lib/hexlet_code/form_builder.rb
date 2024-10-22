486f2434a5e34a16fc761ba71527998a2e81eea5
Suban05 <anatolyb94@gmail.com>
2024-10-22 20:20:13 +0500

    implement `submit`, `label`, `textarea` and refactor `input`

--- lib/hexlet_code/form_builder.rb
+++ lib/hexlet_code/form_builder.rb
@@ -10,14 +10,26 @@ module HexletCode
 
     def input(name, options = {})
       value = @object.public_send(name.to_sym)
+      label(name, name.capitalize)
       if options[:as] == :text
-        cols = options[:cols] || "20"
-        rows = options[:rows] || "40"
-        tag = Tag.build("textarea", { name:, cols:, rows: }.merge(options.except(:as))) { value }
+        textarea(name, value, options)
       else
-        tag = Tag.build("input", { name:, type: options[:type] || "text", value: }.merge(options))
+        add(Tag.build("input", { name:, type: options[:type] || :text, value: }.merge(options)))
       end
-      add(tag)
+    end
+
+    def submit(value = "Save", options = {})
+      add(Tag.build("input", { type: :submit, value: }.merge(options)))
+    end
+
+    def label(input_name, body, options = {})
+      add(Tag.build("label", { for: input_name }.merge(options)) { body })
+    end
+
+    def textarea(name, body, options = {})
+      cols = options[:cols] || "20"
+      rows = options[:rows] || "40"
+      add(Tag.build("textarea", { name:, cols:, rows: }.merge(options.except(:as))) { body })
     end
 
     private
