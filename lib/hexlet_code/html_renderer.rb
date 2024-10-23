640c2a2a90b1d9a805a955457eec655ce5ef61fc
Anatoly Busygin <anatolyb94@gmail.com>
2024-10-23 07:29:12 +0300

    use `map` instead of `each` in `html_renderer`

--- lib/hexlet_code/html_renderer.rb
+++ lib/hexlet_code/html_renderer.rb
@@ -5,31 +5,18 @@ module HexletCode
   class HtmlRenderer
     def initialize(form)
       @form = form
-      @html = []
     end
 
     def render
-      append_items
       Tag.build('form', @form.options) do
-        @html.join
+        @form.items.map do |item|
+          if item.body.nil?
+            Tag.build(item.type, item.options)
+          else
+            Tag.build(item.type, item.options) { item.body.options[:content] }
+          end
+        end.join
       end
-    end
-
-    private
-
-    def append_items
-      @form.items.each do |item|
-        tag = if item.body.nil?
-                Tag.build(item.type, item.options)
-              else
-                Tag.build(item.type, item.options) { item.body.options[:content] }
-              end
-        append(tag)
-      end
-    end
-
-    def append(row)
-      @html << row
     end
   end
 end
